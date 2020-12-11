25cd95ecbbf4eb13885be34f5c0243dfc834b9b1
Glen McCaskill <glen.mccaskill@outlook.com>
2020-12-11 17:11:49 -0600

    Scripting bash with Python

--- lrnimport/intscript02.py
+++ lrnimport/intscript02.py
@@ -2,6 +2,10 @@
 import subprocess  ## <-------- changed
 subprocess.call(["ip", "link", "show", "up"])
 print("This program will check your interfaces.")
-interface = input("Enter an interface, like, ens3: ")
-subprocess.call(["ip", "addr", "show", "dev", interface])  ## <--- changed
-subprocess.call(["ip", "route", "show", "dev", interface]) ## <--- changed
+# interface = input("Enter an interface, like, ens3: ")
+# subprocess.call(["ip", "addr", "show", "dev", interface])  ## <--- changed
+# subprocess.call(["ip", "route", "show", "dev", interface]) ## <--- changed
+print("-"*50)
+subprocess.call(["ping", "-c 4", "8.8.8.8"])
+subprocess.call(["traceroute", "8.8.8.8"])
+subprocess.call(["du","-h", "/home/glen/mycode"])
