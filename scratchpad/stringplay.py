9ac352619540b204d53c3ea8fdfbbd058d452dfb
Glen McCaskill <glen.mccaskill@outlook.com>
2020-12-16 14:10:26 -0600

    usopen and package updates

---
+++ scratchpad/stringplay.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+string01 = "Just a string.\n"
+string02 = "Just another string."
+
+print(string01 + string02)
+
+string01 = string01.rstrip()
+print(string01 + " " + string02)
